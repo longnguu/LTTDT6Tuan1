a88de5a9b6c554bd7172cba695d69b757acee093
longnguu <levanlong29062002@gmail.com>
2022-08-27 19:08:08 +0700

    su kien button back

--- app/src/main/java/com/example/login/ProfileActivity.java
+++ app/src/main/java/com/example/login/ProfileActivity.java
@@ -2,7 +2,10 @@ package com.example.login;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
 
 public class ProfileActivity extends AppCompatActivity {
 
@@ -10,5 +13,13 @@ public class ProfileActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_profile2);
+        final TextView btback = (TextView) findViewById(R.id.btBack);
+        btback.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(ProfileActivity.this,MainActivity.class);
+                startActivity(intent);
+            }
+        });
     }
 }
