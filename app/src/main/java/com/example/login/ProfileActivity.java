f999e61a96a61afd1936754eb45441347dca8642
longnguu <levanlong29062002@gmail.com>
2022-08-27 19:40:26 +0700

    button back

--- app/src/main/java/com/example/login/ProfileActivity.java
+++ app/src/main/java/com/example/login/ProfileActivity.java
@@ -2,8 +2,10 @@ package com.example.login;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.annotation.SuppressLint;
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.MotionEvent;
 import android.view.View;
 import android.widget.TextView;
 
@@ -15,8 +17,10 @@ public class ProfileActivity extends AppCompatActivity {
         setContentView(R.layout.activity_profile2);
         final TextView btback = (TextView) findViewById(R.id.btBack);
         btback.setOnClickListener(new View.OnClickListener() {
+            @SuppressLint("ResourceAsColor")
             @Override
             public void onClick(View view) {
+                btback.setBackgroundColor(R.color.hoverbgcolor);
                 Intent intent = new Intent(ProfileActivity.this,MainActivity.class);
                 startActivity(intent);
             }
